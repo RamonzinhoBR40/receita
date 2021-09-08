b6c40ab95fd1cd23fec01d922428f815c6fcdbee
RamonzinhoBR40 <rranselmo4688@gmail.com>
2021-09-08 14:29:18 -0300

    novo

--- components/menu.js
+++ components/menu.js
@@ -5,7 +5,7 @@ function Menu(){
         <>
            <div>
         <navbar>
-            <Link href='/home'>Receitas rápidas</Link>
+            <a href='/home'>Receitas rápidas</a>
             <input 
             type="text"
             id="pesquisa" 
@@ -36,6 +36,37 @@ function Menu(){
             </ul>
             <hr></hr>
         </p>
+          
+        <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h2>Documentation &rarr;</h2>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h2>Learn &rarr;</h2>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            className={styles.card}
+          >
+            <h2>Examples &rarr;</h2>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+          >
+            <h2>Deploy &rarr;</h2>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div>    
+
         </>
     )
 }
--- pages/home.js
+++ pages/home.js
@@ -1,12 +1,12 @@
 import Menu from '../components/menu'
 import Footer from '../components/footer'
-import Receitas from '../components/receitas'
+
 
 function Home(){
     return (
         <>
         <Menu />
-        <Receitas />
+        
         <Footer />
         </>
     )
