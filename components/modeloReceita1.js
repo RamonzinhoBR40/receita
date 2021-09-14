0e51534160cbae61ee69c2835aed6f96d9afbbb8
RamonzinhoBR40 <rranselmo4688@gmail.com>
2021-09-14 16:59:07 -0300

    receitas

---
+++ components/modeloReceita1.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+function modeloReceita1(){
+    return (
+        <div className={style}>
+            <Image src = "/hamburguer.jpg" width={400} height={280}/>
+        <p>ola mundoooooo!!!</p>
+        
+        </div>
+    )
+}
+
+export default modeloReceita1
--- pages/Receitas1.js
+++ pages/Receitas1.js
@@ -1,13 +1,14 @@
 import Menu from '../components/menu'
 import Footer from '../components/footer'
 import Receita from '../components/receita'
+import Modelo1 from '../components/modeloreceita1'
 
 
 function Home(){
     return (
         <>
         <Menu />
-        <Receita />
+        <Modelo1 />
         <Footer />
         </>
     )
