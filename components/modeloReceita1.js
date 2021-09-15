523bb85e7eb3fefc6ff5831d03572ebc3b141a26
RamonzinhoBR40 <rranselmo4688@gmail.com>
2021-09-15 15:05:11 -0300

    commit

--- components/modeloReceita1.js
+++ components/modeloReceita1.js
@@ -1,34 +1,38 @@
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Components.module.css'
 
 function modeloReceita1(){
     return (
         <div className={styles.receita}>
-            <Image src = "/hamburguer.jpg" width={400} height={280}/>
+            <div className={styles.image}> 
+                <p><h2>Hamburguer de Frango</h2></p>           
+                <Image src = "/hamburguer.jpg" width={390} height={270}/>                            
+            </div>
+
             <div className={styles.texto}>
-                <p>Hamburguer de Frango</p>
-                <ul>Ingredientes: <br />
+                
+                <ul><h2>Ingredientes: </h2><br />
 
-                400 g de peito de frango<br />
-                Sal a gosto<br />
-                Pimenta-do-reino a gosto<br />
-                Tempero de sua preferência a gosto<br />
-                1 cebola ralada<br />
-                5 colheres de sopa de farinha de aveia</ul>
+                <li>400 g de peito de frango</li><br />
+                <li>Sal a gosto</li><br />
+                <li>Pimenta-do-reino a gosto</li><br />
+                <li>Tempero de sua preferência a gosto</li><br />
+                <li>1 cebola ralada</li><br />
+                <li>5 colheres de sopa de farinha de aveia</li></ul>
 
-                <ul>Modo de preparo: <br />
+                <ul><h2>Modo de preparo:</h2> <br />
 
-                1-Em um processador, coloque o peito de frango sem osso e triture.<br />
+                <li>Em um processador, coloque o peito de frango sem osso e triture.</li><br />
 
-                2-Transfira para um recipiente, tempere com sal, pimenta e outros temperos que preferir (alho ralado, chimichurri, pimenta caiena, páprica…).<br />
+                <li>Transfira para um recipiente, tempere com sal, pimenta e outros temperos que preferir (alho ralado, chimichurri, pimenta caiena, páprica…).</li><br />
 
-                3-Adicione a cebola picada, uma colher de sopa da farinha de aveia e misture bem.<br />
+                <li>Adicione a cebola picada, uma colher de sopa da farinha de aveia e misture bem.</li><br />
 
-                4-Molde a massa em formato de hambúrguer. Se preferir, use uma balança para deixar todos com a mesma quantidade.<br />
+                <li>Molde a massa em formato de hambúrguer. Se preferir, use uma balança para deixar todos com a mesma quantidade.</li><br />
 
-                5-Após fazer os hambúrgueres, empane cada um na farinha de aveia.<br />
+                <li>Após fazer os hambúrgueres, empane cada um na farinha de aveia.</li><br />
 
-                6-Congele ou frite na hora.</ul>
+                <li>Congele ou frite na hora.</li></ul>
             </div>
         </div>
     )
